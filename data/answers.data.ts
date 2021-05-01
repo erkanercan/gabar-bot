85c2b644bd62e24e4d561f75702ea6733bc64a67
erkanercan <erkan.ercan@fill-labs.com>
2021-05-01 23:34:09 +0300

    gabar bot ready to serve

---
+++ data/answers.data.ts
@@ -0,0 +1,43 @@
+export const selamAnswers = [
+  "Yetersiz",
+  "Hacım",
+  "His ve Araştırma",
+  "Selam hacım 😉",
+  "😉",
+  "Hocam hayırlı günler",
+  "Son Günlerde yaşanan hadiselerden dolayı, anlayışla karşılamak lazım",
+  "NEVIR PİEM FÖRST",
+];
+
+export const naberAnswers = [
+  "Audit geliyor hocam",
+  "Çok satış yiyoruz",
+  "Alıcı az",
+  "Ufak çapta bi mod luk verebiliyor musun hacım",
+  "3/5 bişey katmaya çalıştığınız gibi,  bizde yatırımcı kitlesi oluşturuyoruz.. Macaronun tanıtımını yapmaktan işimi yapmaz oldum :)",
+  "🤔🤔",
+  "hmm",
+];
+export const bizzatAnswers = [
+  "Binance ✍️",
+  "PancakeSwap",
+  "MXC",
+  "ben ✍️",
+  "Luke",
+  "Gabar ✍️",
+  "Anan",
+  "Makaron ağı",
+  "✍️✍️",
+  "✍️✍️✍️",
+];
+
+export const replikAnswers = [
+  "Relax arkadaşlar relax",
+  "Makaronlarınıza sahip çıkın",
+  "Hacım yeni haber var mı 🤔🤔🤔",
+  "Binance bizzat gelecek",
+  "But gabar is best bot to telegram -Bir Dost",
+  "Makaronun bizzat kendi ağı olacak",
+];
+
+export const iyiBiriAnswers = ["Yasemin", "Sen", "Ben", "Luke", "Chef", "Cem", "Satoshi"];
---
+++ index.ts
@@ -0,0 +1,78 @@
+import { Telegraf } from "telegraf";
+import signale from "signale";
+import {
+  bizzatAnswers,
+  selamAnswers,
+  naberAnswers,
+  replikAnswers,
+  iyiBiriAnswers,
+} from "./data/answers.data";
+
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.GABAR_BOT_TOKEN);
+// /selam command
+bot.command("selam", (ctx: any) => {
+  signale.info("Sending selam answer to @" + ctx?.from?.username + " at " + new Date());
+
+  ctx.telegram.sendMessage(
+    ctx.message.chat.id,
+    selamAnswers[Math.floor(Math.random() * selamAnswers.length)],
+  );
+});
+
+// /naber command
+bot.command("naber", (ctx: any) => {
+  signale.info("Sending naber answer to @" + ctx?.from?.username + " at " + new Date());
+  ctx.telegram.sendMessage(
+    ctx.message.chat.id,
+    naberAnswers[Math.floor(Math.random() * naberAnswers.length)],
+  );
+});
+
+// /bizzat command
+bot.command("bizzat", (ctx: any) => {
+  signale.info("Sending bizzat answer to @" + ctx?.from?.username + " at " + new Date());
+
+  ctx.telegram.sendMessage(
+    ctx.message.chat.id,
+    `Bizzat ${bizzatAnswers[Math.floor(Math.random() * bizzatAnswers.length)]}`,
+  );
+});
+
+// /replik command
+bot.command("replik", (ctx: any) => {
+  signale.info("Sending replik answer to @" + ctx?.from?.username + " at " + new Date());
+
+  ctx.telegram.sendMessage(
+    ctx.message.chat.id,
+    replikAnswers[Math.floor(Math.random() * replikAnswers.length)],
+  );
+});
+
+// /iyibiri command
+bot.command("iyibiri", (ctx: any) => {
+  signale.info("Sending iyibiri answer to @" + ctx?.from?.username + " at " + new Date());
+
+  ctx.telegram.sendMessage(
+    ctx.message.chat.id,
+    `${iyiBiriAnswers[Math.floor(Math.random() * iyiBiriAnswers.length)]} iyi biri`,
+  );
+});
+
+try {
+  bot.launch();
+  signale.success("Bot started.");
+} catch (error) {
+  signale.fatal(error);
+}
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+//selam - Gabarizm size bir selam verir. İyi gününüzdesiniz.
+//naber - Gabar'a bir naber kartı oynayıp engin bilgilerinden yararlanmaya çalışırsınız.
+//bizzat - Gabar ile bizzat kimin geleceğini tartışırsınız.
+//replik - GabarBot  size bir Gabar repliği fırlatır.
+//iyibiri - Kimin iyi biri olduğuna karar veremiyor musunuz? Bırakın Gabar sizin yerinize karar versin.
